3854145dc425c347cd05d6297b809e19369ade0d
Qaamuus Academy <qaamuusplatform@gmail.com>
2023-01-17 18:23:42 +0300

    first commit

--- efamliy_tree/settings.py
+++ efamliy_tree/settings.py
@@ -92,12 +92,25 @@ WSGI_APPLICATION = 'efamliy_tree.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("NAME"),
+        'USER': config("USER"),
+        'PASSWORD': config("PASSWORD"),
+        'HOST': config("HOST"),
+        'PORT': config("PORT"),
     }
 }
+
 
 
 # Password validation
